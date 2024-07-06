1c34dd70ae57fb1ca2c1b6f5d0c5395f96298837
Cleberson Rocha <clebersonrochaads@gmail.com>
2024-07-05 21:44:22 -0300

    Feat: Add the base url

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
+<?php
+    require_once("globals.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MovieStar</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="shortcut icon" href="<?= $BASE_URL ?>img/moviestar.ico">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.css" integrity="sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
 </head>
 <body>
     <h1>MovieStar Test</h1>
